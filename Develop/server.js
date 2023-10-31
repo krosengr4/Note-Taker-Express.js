3457335f27297bc5dd3920af36d15c6b3b9f0d76
krosengr4 <rosenkev4@gmail.com>
2023-10-31 17:01:02 -0500

    integrated middlewear on got index to show on my server

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,39 @@
+//! This file contains the server data.
+
 const express = require('express'); 
 const fs = require('fs');
 const path = require('path');
 
-const userNotes = [];
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const PORT = 3000;
+//* Middleware for parsing application/json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+const userNotes = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+
+
+
+// const allNotes = require('./db/db.json');
+// app.get('/api/notes', (req, res) => {
+//     res.json
+// })
+
+// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'))
+// });  
+
+app.listen(PORT, () =>
+  console.log(`port is http://localhost:${PORT}`)
+);
