b4bf0f516ec080717f0df213265b28152af035b7
krosengr4 <rosenkev4@gmail.com>
2023-11-01 03:19:32 -0500

    Got data written in notes app to write to db.json Added helper folder (fsUtils.js).

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,13 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+//* Helper functions from /fsUtils.js
+const {
+  readFromFile,
+  writeToFile, 
+  readAndAppend,
+} = require('./helpers/fsUtils');
+
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -17,31 +24,58 @@ app.use(express.static('public'));
 const userNotes = [];
 const allNotes = require('./db/db.json')
 
+
 //* Homepage whenever you open the localhost ("localhost:PORT/")
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-    console.log(`Hello! ${req.method}Welcome to Express Note Taker!`)
+  res.sendFile(path.join(__dirname, './public/index.html'))
+  console.log(`Hello! ${req.method}Welcome to Express Note Taker!`)
 });
 
 //* The /notes page when "Get Started is clicked" (localhost:PORT/notes)  
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-
-    console.log(`${req.method} request recieved to go to /notes.`)
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+  
+  console.log(`${req.method} request recieved to go to /notes.`)
 });
 
 //* This shows the notes that are in db.json file.
 app.get('/api/notes', (req, res) => {
-  res.json(allNotes); //<--- Might need "res.json(allNotes.slice(1));"
+  res.json(allNotes); //! <--- Add ".slice(1)" to "(allNotes)"
 });
+
 
 //* Default page. Handles invalid /* cases. Returns 404 page.
 app.get('*', (req, res) => 
   res.sendFile(path.join(__dirname, 'public/404.html'))
 );
 
+//// userNotes = fs.readFileSync("db/db.json", "utf-8");
+//// userNotes = JSON.parse(userNotes);
+//// res.json(userNotes);
+
 // TODO: Create code that saves new notes to db.json
+app.post('/api/notes', (req,res) => {
+  console.info(`${req.method} request recieved to add a note`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json('new note added sucessfully!');
+  } else {
+    res.error('Error with adding your note')
+  } 
+});
+
+
 // TODO: Create code that displays saved notes in db.json
+
+
 // TODO: Create code that deletes a note from db.json when user selects delete. 
 // TODO: Create code that displays that the note has been deleted.
 
